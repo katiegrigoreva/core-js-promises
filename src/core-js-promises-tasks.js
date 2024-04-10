691ba24c9bff85b7fbeb49ea916131b81581ffde
Kate <e.khizova@gmail.com>
2024-04-10 13:24:39 +0300

    feat: add solutions

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -85,8 +85,10 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return Promise.race(promises).then((value) => {
+    return value;
+  });
 }
 
 /**
@@ -100,8 +102,10 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises).then((values) => {
+    return values;
+  });
 }
 
 /**
